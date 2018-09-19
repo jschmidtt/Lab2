eb39c524849b77d7d468a71342eb455017735d70
justinschmidt <justinschmidttt@gmail.com>
2018-09-19 09:44:08 -0400

    Sign up form does all valid checks.

--- app/src/main/java/edu/temple/lab2/FormActivity.java
+++ app/src/main/java/edu/temple/lab2/FormActivity.java
@@ -2,7 +2,10 @@ package edu.temple.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,6 +13,9 @@ public class FormActivity extends AppCompatActivity {
     EditText textEmail;
     EditText textPassword;
     EditText textPasswordConfirm;
+    Button buttonSave;
+
+    final private int REQUEST_CODE_NAME_DISPLAY = 934;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +23,43 @@ public class FormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_form);
         setTitle("SignUpForm");
 
+        //SetupFindViews
         textName = findViewById(R.id.textName);
         textEmail = findViewById(R.id.textEmail);
         textPassword = findViewById(R.id.textPassword);
         textPasswordConfirm = findViewById(R.id.textPasswordConfirm);
+        buttonSave = findViewById(R.id.buttonSave);
 
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean start = true;
+
+                String name, email, password, passwordConfirm;
+
+                //get the strings from the textEdits
+                name = textName.getText().toString();
+                email =  textEmail.getText().toString();
+                password = textPassword.getText().toString();
+                passwordConfirm = textPasswordConfirm.getText().toString();
+
+                //check to see if any fields are empty
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()) {
+                    Toast empty = Toast.makeText(getApplicationContext(), "All fields must be entered.", Toast.LENGTH_LONG);
+                    empty.show();
+                    start = false;
+                }
+                //Check to see if the passwords match or not
+                if (!password.equals(passwordConfirm) || !passwordConfirm.equals(password)) {
+                    Toast passwordMismatch = Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_LONG);
+                    passwordMismatch.show();
+                    start = false;
+                }
+
+                //Launch DisplayActivity if all good
+
+
+            }
+        });
     }
 }
